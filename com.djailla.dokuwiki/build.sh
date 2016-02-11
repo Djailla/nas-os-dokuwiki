5b89b6ac5596586a2326f142ab5919954c945318
Djailla <bastien.vallet@gmail.com>
2016-02-11 14:24:12 +0100

    Cleanup build script and package info

--- com.djailla.dokuwiki/build.sh
+++ com.djailla.dokuwiki/build.sh
@@ -5,17 +5,19 @@ mkdir -m 755 -p /opt/dokuwiki/
 # Install dependencies
 apt-get update
 apt-get install -y -q apache2 libapache2-mod-php5
-# apt-get install -y -q dokuwiki
 
+# Checkout the source code of dokuwiki
 mkdir -p /tmp/dokuwiki
 cd /tmp/dokuwiki
 wget http://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz
 tar xvf dokuwiki-stable.tgz
 
+# Move to www folder
 mv /tmp/dokuwiki/dokuwiki-*/* /var/www
 chown -R www-data:www-data /var/www
 rm -rf /tmp/dokuwiki
 
+# Install scripts
 install -m 755 /home/source/rc.local /etc
 install -m 644 /home/source/default.apache_template /etc/default.apache_template
 install -m 644 /home/source/ports.conf.apache_template /etc/ports.conf.apache_template
